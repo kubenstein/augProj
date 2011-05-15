130279fcdf222bf61ea3635138c4cbbd71f73a2e
Kuba <s6052@pjwstk.edu.pl>
2011-05-15 18:34:37 +0200

    obsluga kopiowania wartosci miedzy dwoma zmiennymi stringowymi

--- zmienneStringowe.y.c
+++ zmienneStringowe.y.c
@@ -24,7 +24,13 @@ void stringVar_assign_string( char* s1, char* s2, char* s3 ) {
 
 
 void stringVar_assign_stringVar( char* s1, char* s2, char* s3 ) {
-	// ...
+	char ids1[10];
+	sscanf(s1, "%*s stringZm_%s",ids1);
+
+	printf("%s;",s1);
+	printf("if( stringZm_%s ) free(stringZm_%s);",ids1,ids1);
+	printf("stringZm_%s = malloc( sizeof(char) * strlen(%s));",ids1,s3);
+	printf("strcpy( stringZm_%s,%s);",ids1,s3);
 	free(s1);
 	free(s2);
 	free(s3);
