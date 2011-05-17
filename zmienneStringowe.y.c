e266adf4959fca692a62b5ebb13a6bbe13240239
Kuba <s6052@pjwstk.edu.pl>
2011-05-17 19:26:36 +0200

    literowka w funkcji bledu

--- zmienneStringowe.y.c
+++ zmienneStringowe.y.c
@@ -2,7 +2,7 @@
 
 /* funkcja bledow */
 void noStringVarError( long int idZmiennej ) {
-	fprintf( stdout, "ERR_COMPILE: int variable never initialized: #%06x\n", idZmiennej );
+	fprintf( stdout, "ERR_COMPILE: string variable never initialized: #%06x\n", idZmiennej );
 	exit(1);
 }
 
